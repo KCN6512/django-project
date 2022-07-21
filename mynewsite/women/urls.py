4bde7a8e1a3572f293a0492cfea32b5d26c908c2
Alexander <troshiy2011@mail.ru>
2022-07-21 21:07:02 +0300

    added regexp

--- mynewsite/women/urls.py
+++ mynewsite/women/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import *
 
 urlpatterns = [
     path('',mainpage),
     path('cats/<int:catid>/',categories),
+    re_path(r'^archive/(?P<year>[0-9]{4})/',archive)
 ]
--- mynewsite/women/views.py
+++ mynewsite/women/views.py
@@ -4,8 +4,13 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse('Страница приложения women')
 
-def categories(request):
-    return HttpResponse('<h1>Статьи по категориям </h1>')
+def categories(request,catid):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
 
 def mainpage(request):
     return HttpResponse('Главная страница сайта')
+
+def archive(request,year):
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}<p>')
