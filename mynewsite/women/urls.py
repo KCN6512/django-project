06db03cb4fa33365c10fc70e3663e6aeff63f8f4
Alexander <troshiy2011@mail.ru>
2022-07-25 20:02:15 +0300

    nichego ne ponyal

--- mynewsite/women/urls.py
+++ mynewsite/women/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', women_mainpage, name='women-home'),
-    path('cats/<int:catid>/', categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
-    path('about/', about, name='about')
+    path('', index,name='home'),
+    path('about/', about, name='about'),
+    path('add_page/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>',show_post,name='post'),
 ]
--- mynewsite/women/views.py
+++ mynewsite/women/views.py
@@ -3,27 +3,41 @@ from django.shortcuts import redirect, render
 from .models import *
 
 
-menu = ['О сайте','Добавить статью','Обратная связь','Войти']
+menu = [
+    {'title': "О сайте",'url_name':'about'},
+    {'title': "Добавить статью",'url_name':'add_page'},
+    {'title': "Обратная связь",'url_name':'contact'},
+    {'title': "Войти",'url_name':'login'},
+]
 
-def categories(request,catid):
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
 
-def mainpage(request):
+def index(request):
     posts = Women.objects.all()
-    return render(request,'women/index.html',{'posts':posts,'title': 'Главная страница','menu': menu})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request,'women/index.html',context=context)
 
 def about(request):
     return render(request,'women/about.html',{'title': 'О сайте','menu': menu})
 
-def women_mainpage(request):
-    return HttpResponse('Главная страница раздела women')
-
-def archive(request,year):
-    if int(year) > 2022:
-        return redirect('women-home')
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}<p>')
 
 def page_not_found(request,exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+def addpage(request):
+    return HttpResponse('Добавление статьи')
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+def addpage(request):
+    return HttpResponse('Добавление статьи')
+
+def login(request):
+    return HttpResponse('Авторизация')
+
+def show_post(request,post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
