dbd5019ba8b04657f954d9f1da8866f3e66c56b7
Alexander <troshiy2011@mail.ru>
2022-07-26 16:49:11 +0300

    category model and pictures of it

--- mynewsite/women/models.py
+++ mynewsite/women/models.py
@@ -9,6 +9,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category',on_delete=models.PROTECT)
 
     def __str__(self) -> str:
         return self.title
@@ -16,3 +17,9 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse("post", kwargs={"post_id": self.pk})
     
+
+class Category(models.Model):
+    name = models.CharField(max_length=100,db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
