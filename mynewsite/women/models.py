6056981ebe7eef4885c4ade68ec6a9369d1821d7
Alexander <troshiy2011@mail.ru>
2022-07-26 17:42:51 +0300

    maked migrations for new model

--- mynewsite/women/models.py
+++ mynewsite/women/models.py
@@ -9,7 +9,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category',on_delete=models.PROTECT)
+    cat = models.ForeignKey('Category',on_delete=models.PROTECT,null=True)
 
     def __str__(self) -> str:
         return self.title
