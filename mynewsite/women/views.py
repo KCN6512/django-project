59e1fa12501794c93596ea7884b2a24b8a57c84d
Alexander <troshiy2011@mail.ru>
2022-07-26 21:37:16 +0300

    govno nichego ne ponyatno,added time and category name to index.html

--- mynewsite/women/views.py
+++ mynewsite/women/views.py
@@ -47,4 +47,17 @@ def show_post(request,post_id):
     return HttpResponse(f'Отображение статьи с id = {post_id}')
 
 def show_category(request,cat_id):
-    return HttpResponse(f'Отображение категории с id = {cat_id}')
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if not posts:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats' : cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+    return render(request,'women/index.html',context=context)
