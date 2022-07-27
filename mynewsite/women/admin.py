4910559de00fba79dd07afa5ba8ad3ae2d611d79
Alexander <troshiy2011@mail.ru>
2022-07-27 14:33:41 +0300

    added verbose_name to mdoels class Meta modified apps and admin

--- mynewsite/women/admin.py
+++ mynewsite/women/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Women) #регистрация в админ панели модели Women
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id','title','time_create','photo','is_published')
+    list_display_links = ('id','title')
+    search_fields = ('title','content')
+
+admin.site.register(Women,WomenAdmin) #регистрация в админ панели модели Women
--- mynewsite/women/models.py
+++ mynewsite/women/models.py
@@ -3,19 +3,27 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    time_create = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255,verbose_name='Заголовок')
+    content = models.TextField(blank=True,verbose_name='Контент')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/',verbose_name='Фото')
+    time_create = models.DateTimeField(auto_now_add=True,verbose_name='Дата создания')
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category',on_delete=models.PROTECT,null=True)
+    is_published = models.BooleanField(default=True,verbose_name='Опубликовано')
+    cat = models.ForeignKey('Category',on_delete=models.PROTECT,null=True,verbose_name='Категория')
 
     def __str__(self) -> str:
         return self.title
 
     def get_absolute_url(self):
         return reverse("post", kwargs={"post_id": self.pk})
+
+    class Meta: #класс meta нужен для админ панели
+        verbose_name = 'Известные актеры' #название в единственном числе с добавлением s в коцне
+        verbose_name_plural = 'Известные актеры' #название в множественном числе
+        ordering = ['title','time_create'] #сортировка по параметрам по порядку||работает не только для админки но и для сайта
+        #если значения равные то сортируется по последующим параметрам||
+        # мождно указать - в начале элемента списка чтобы сортировка пошла наоборот'''
+
     
 
 class Category(models.Model):
