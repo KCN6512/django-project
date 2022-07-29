b99d5dffaacefd5bd6d920ff96b041cfe95c077b
Alexander <troshiy2011@mail.ru>
2022-07-29 18:53:39 +0300

    added user tags

--- mynewsite/women/templatetags/women_tags.py
+++ mynewsite/women/templatetags/women_tags.py
@@ -4,6 +4,17 @@ from women.models import *
 register = template.Library() #создание экземпляра класса билблиотеки через который будет происходить регистрация тэгов
 
 @register.simple_tag(name='getcats')#декоратор регистарции | простой тэг
-def get_categories():#теперь тэг
-    return Category.objects.all()
+def get_categories(filter=None):#теперь тэг
+    if not filter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
 
+@register.inclusion_tag(filename='women/list_categories.html')
+def show_categories(sort=None,cat_selected=0):
+    if not sort:
+        cats = Category.objects.all()
+    else: 
+        cats = Category.objects.order_by(sort)
+
+    return {'cats':cats,'cat_selected':cat_selected}
