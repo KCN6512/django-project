934383653bb92b11863a3413c49791a54a997db1
Alexander <troshiy2011@mail.ru>
2022-08-02 14:44:09 +0300

    minor

--- mynewsite/women/forms.py
+++ mynewsite/women/forms.py
@@ -6,16 +6,17 @@ from django.core.exceptions import ValidationError
 class AddPostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['cat'].empty_label = 'Категория не выбрана'
+        self.fields['cat'].empty_label = 'Категория не выбрана' #все это для того чтобы переименовать пустой параметр
 
     class Meta:
         model = Women
-        fields = ['title','slug','content','photo','is_published','cat']
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat']
         widgets = {
-            'title': forms.TextInput(attrs={'class':'form-input'}),
-            'content': forms.Textarea(attrs={'cols':60,'rows':10}),
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
 
         }
+
     def clean_title(self): #начинается с clean проверяет _title
         title = self.cleaned_data['title']
         if len(title) > 200:
