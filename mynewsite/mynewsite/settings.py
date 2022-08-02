d6541cc2d2beabc7f2da2ec286c5b1854f904e74
Alexander <troshiy2011@mail.ru>
2022-08-02 18:42:01 +0300

    env minors

--- mynewsite/mynewsite/settings.py
+++ mynewsite/mynewsite/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY',default='')# secret key in .env
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
