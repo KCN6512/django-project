a683a82ed9bee94b7665465779304f3cb762d57d
Alexander <troshiy2011@mail.ru>
2022-08-02 17:03:45 +0300

    removed commented views

--- mynewsite/women/views.py
+++ mynewsite/women/views.py
@@ -7,8 +7,6 @@ from .forms import *
 from .models import *
 
 
-
-
 class WomenHome(ListView):
     model = Women
     template_name = 'women/index.html'
@@ -23,27 +21,6 @@ class WomenHome(ListView):
     def get_queryset(self): # фильтр queryset
         return Women.objects.filter(is_published=True)
 
-# def index(request):
-#     posts = Women.objects.filter(is_published=True)
-#     context = {
-#         'posts': posts,
-#         'title': 'Главная страница',
-#         'cat_selected': 0,
-#     }
-#     return render(request,'women/index.html',context=context)
-    
-# def show_category(request,cat_id):
-#     posts = Women.objects.filter(cat_id=cat_id)
-
-#     if not posts:
-#         raise Http404()
-
-#     context = {
-#         'posts': posts,
-#         'title': 'Отображение по рубрикам',
-#         'cat_selected': cat_id,
-#     }
-#     return render(request,'women/index.html',context=context)
 
 class WomenCategory(ListView):
     model = Women
@@ -66,16 +43,6 @@ def about(request):
 def page_not_found(request,exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
-# def add_page(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
-#     else:
-#         form = AddPostForm()
-#     return render(request,'women/addpage.html',{'form':form,'title':'Добавление статьи'})
-
 class AddPage(CreateView):
     form_class = AddPostForm
     template_name = 'women/addpage.html'
@@ -96,17 +63,6 @@ def addpage(request):
 
 def login(request):
     return HttpResponse('Авторизация')
-
-# def show_post(request,post_id): #post_slug
-#     post = get_object_or_404(Women, pk=post_id) #slug=post_slug
-
-#     context = {
-#         'post': post,
-#         'title': post.title,
-#         'cat_selected': post.cat_id,
-#     }
-
-#     return render(request,'women/post.html',context=context)
 
 class ShowPost(DetailView):
     model = Women
