e9a5bedd4f4b6d6737d76041e93240cb31fc70e2
Alexander <troshiy2011@mail.ru>
2022-08-04 14:44:39 +0300

    added commands and added sql logging to console in settings

--- mynewsite/mynewsite/settings.py
+++ mynewsite/mynewsite/settings.py
@@ -54,6 +54,28 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+LOGGING = {#для вывода sql запросов в консоль
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
+
 ROOT_URLCONF = 'mynewsite.urls'
 
 TEMPLATES = [
