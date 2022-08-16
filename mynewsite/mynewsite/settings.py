7e0a6b70453f90358ed26d4fa9209add0aed7533
Alexander <troshiy2011@mail.ru>
2022-08-16 15:52:57 +0300

    minor

--- mynewsite/mynewsite/settings.py
+++ mynewsite/mynewsite/settings.py
@@ -26,12 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY',default='')# secret key in .env
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
-
 
 # Application definition
 
