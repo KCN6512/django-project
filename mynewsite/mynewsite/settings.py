c8c75368bb9101a0ca44858d60d50cd7ef3833c4
Alexander <troshiy2011@mail.ru>
2022-09-17 16:21:03 +0300

    login url default

--- mynewsite/mynewsite/settings.py
+++ mynewsite/mynewsite/settings.py
@@ -10,12 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from audioop import reverse
 import os
 from email.policy import default
 from pathlib import Path
 
 import debug_toolbar
 from decouple import config
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -177,3 +179,5 @@ CACHES = {
 
 CAPTCHA_FONT_SIZE = 30
 CAPTCHA_IMAGE_SIZE = (150,50)
+
+LOGIN_URL = reverse_lazy('login')
