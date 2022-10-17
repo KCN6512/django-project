a99c53fcaa66b4e9e58b8560f423fd846d5ea2fe
Alexander <troshiy2011@mail.ru>
2022-10-17 19:01:27 +0300

    inline and admin

--- mynewsite/actor/admin.py
+++ mynewsite/actor/admin.py
@@ -3,6 +3,14 @@ from django.utils.safestring import mark_safe
 
 from .models import *
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+    list_display_links = ('id','name')
+    search_fields = ('name',) #нужно передать кортеж поэтому запятая,
+
+class CategoriesInline(admin.TabularInline):
+    model = Actor.cat.through
+    extra = 1
 
 class ActorAdmin(admin.ModelAdmin):
     list_display = ('id','title','time_create','get_html_photo','is_published') # gethtmlphoto тобразить фотографии
@@ -12,7 +20,8 @@ class ActorAdmin(admin.ModelAdmin):
     list_filter = ('is_published','time_create')
     #prepopulated_fields = {'slug': ('title',)} используется для автосоздания slug'a
     fields = ('title', 'slug', 'cat', 'content', 'photo', 'get_html_photo', 'is_published', 'time_create', 'time_update', 'likes')# поля выводимые в админку при редактировании | только после readonly_fields можно жсюда добавить 'time_create', 'time_update'
-    readonly_fields = ('time_create', 'time_update', 'get_html_photo')#нередактируемые поля
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo', 'slug', 'likes')#нередактируемые поля
+    inlines = [CategoriesInline] #удобный выбор категория inline
 
     def get_html_photo(self, object):
         if object.photo:
@@ -21,10 +30,6 @@ class ActorAdmin(admin.ModelAdmin):
     get_html_photo.short_description = 'Мини фото'
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id','name')
-    list_display_links = ('id','name')
-    search_fields = ('name',)#нужно передать кортеж поэтому запятая,
 
 admin.site.register(Actor,ActorAdmin) #регистрация в админ панели модели Actor
 admin.site.register(Category,CategoryAdmin)
